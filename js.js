60738c97ed07894042a922ee18f50ad732c5a7e0
pavel-oliinyk <super.poul@yandex.ru>
2017-06-29 21:36:08 +0300

    first commit

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$("form").submit(function(){
+	$("#form").submit(function(){
 		$.ajax({
 			type: = "POST",
 			url: "mail.php",
@@ -8,7 +8,7 @@ $(document).ready(function(){
 		}).done(function(){
 			$(this).find("input").val("");
 			$('#succes-block').fadeIn(500);
-			$("form").trigger("reset");
+			$("#form").trigger("reset");
 		});
 		return false;
 	});
